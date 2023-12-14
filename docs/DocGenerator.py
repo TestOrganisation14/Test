f131aad4b7842e0dc49aecb46aa70849bc864d0d
raj-1513 <rajkumar.shah@amagi.com>
2023-12-14 16:30:13 +0530

    changes in DocGenerator

--- docs/DocGenerator.py
+++ docs/DocGenerator.py
@@ -1,10 +1,9 @@
 opsConfigFieldsDocMap = [
 {
     "key"   : "PlaMode",
-    "scope" : ["pla","plafull","dd","tr","trfull","raj"],
+    "scope" : ["pla","plafull","dd","tr","trfull"],
     "doc"   : F("""\
                 Description
-                    Player software supports multiple modes of operation.
                     This field configures the software to a specific operational mode.
 
                 Supported values
@@ -15,6 +14,40 @@ opsConfigFieldsDocMap = [
                 This is a mandatory field.
                 Example
                     PlaMode = dd""")
+},
+{
+    "key"   : "PlaMode",
+    "scope" : ["player","playerfull","doordarshan","transcoder","transcoderfull"],
+    "doc"   : F("""\
+                Description
+                    This field configures the software to a specific operational mode.
+
+                Supported values
+                * pl
+                * dd
+                * tr
+
+                This is a mandatory field.
+                Example
+                    PlaMode = dd""")
+},
+{
+    "key"   : "EnableOpsConfig",
+    "scope" : ["pla","plafull","dd","tr","trfull"],
+    "doc"   : F("""\
+                Description
+                    The configuration enables or disables usage of CMS
+                    software for generation Cloudport Player configurations.
+
+                Supported values
+                * true
+                * false
+
+                This is a mandatory field.
+                When in doubt, configure to true.
+
+                Example
+                    EnableOpsConfig = true""")
 },
 {
     "key"   : "EnableOpsConfig",
@@ -35,21 +68,36 @@ opsConfigFieldsDocMap = [
                     EnableOpsConfig = true""")
 },
 {
-    "key"   : "EnableOpsConfig",
-    "scope" : ["pla","plafull","dd","tr","trfull"],
+    "key"   : "BlipFeedResolution",
+    "scope" : ["pla","plafull"],
     "doc"   : F("""\
                 Description
-                    The configuration enables or disables usage of CMS
-                    software for generation Cloudport Player configurations.
+                    Configuration for resolution and frame rate of Blip in Player.
+                    This field is used to create filenames of IMT and DVB subtitle
+                    files.
+
+                Format
+                    <width>x<height><scantype><framerate>
+
+                    Please note the 'x' between width and height.
+
+                    where,
+                        * width       : pixel width of video frames
+                        * height      : pixel height of video frames
+                        * scantype    : video frame scan type.
+                                        Supported types are i(interlaced) and p(progressive)
+                        * framerate   : Video frames per second
 
                 Supported values
-                * true
-                * false
+                * 720x576i50        (DVB,IMT)
+                * 720x480i59.94     (DVB)
+                * 1920x1080i50      (DVB)
+                * 1920x1080i59.94   (DVB)
 
-                This is a mandatory field.
-                When in doubt, configure to true.
+                This field is mandatory if IMT or DVB subtitle file playback
+                feature is needed.
 
                 Example
-                    EnableOpsConfig = true""")
+                    BlipFeedResolution = 720x576i50""")
 }
 ]
